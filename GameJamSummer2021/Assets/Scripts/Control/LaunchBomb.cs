0efd41e9a01cb2c7328449d83d755164c763beee
tjjoris <tjjoris@gmail.com>
2021-06-27 20:35:28 -0400

    bombs have velocity out the rear.

--- GameJamSummer2021/Assets/Scripts/Control/LaunchBomb.cs
+++ GameJamSummer2021/Assets/Scripts/Control/LaunchBomb.cs
@@ -8,14 +8,17 @@ namespace FreeEscape.Control
     {
         [SerializeField] private GameObject bombPrefab;
         private Rigidbody2D rigidbody2D;
+        private float reverseVelocity = 100f;
         private void Start()
         {
             rigidbody2D = GetComponent<Rigidbody2D>();
         }
         public void LaunchBombAction()
         {
-            GameObject bomb = Instantiate(bombPrefab, transform.position, Quaternion.identity);
+            GameObject bomb = Instantiate(bombPrefab, transform.position, transform.rotation);
             Vector2 shipVelocity = rigidbody2D.velocity;
+            Vector2 reverseV2 = new Vector2(0, -reverseVelocity);
+            bomb.GetComponent<Rigidbody2D>().AddRelativeForce(reverseV2);
             bomb.GetComponent<Rigidbody2D>().velocity = shipVelocity;
         }
     }
