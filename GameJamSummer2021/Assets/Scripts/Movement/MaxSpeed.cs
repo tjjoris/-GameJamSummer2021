eb0d82a1a469def4d9f48f85d8626f65f77bbcf9
tjjoris <tjjoris@gmail.com>
2021-07-02 14:44:45 -0400

    Implementing max speed part 1
    max speed seems to be implemented, but it's hard to tell its 100% perfect with the glare outside.

--- GameJamSummer2021/Assets/Scripts/Movement/MaxSpeed.cs
+++ GameJamSummer2021/Assets/Scripts/Movement/MaxSpeed.cs
@@ -7,14 +7,25 @@ namespace FreeEscape.Movement
 
     public class MaxSpeed : MonoBehaviour
     {
+        private Rigidbody2D rb;
         private Vector2 rbVelocity;
         private float speed;
         private float maxSpeed = 10f;
+        private float radiansOfVelocity;
         void Update()
         {
-            rbVelocity = GetComponent<Rigidbody2D>().velocity;
+            rb = GetComponent<Rigidbody2D>();
+            rbVelocity = rb.velocity;
             //Debug.Log("rb velocity source " + rbVelocity.ToString());
             speed = rbVelocity.magnitude;
+            radiansOfVelocity = Mathf.Atan2(rbVelocity.y, rbVelocity.x);
+            //angleOfVelocity = angleOfVelocity * Mathf.Rad2Deg;
+            if (speed > maxSpeed)
+            {
+                float xSpeed = Mathf.Cos(radiansOfVelocity) * maxSpeed;
+                float ySpeed = Mathf.Sin(radiansOfVelocity) * maxSpeed;
+                rb.velocity = new Vector2(xSpeed, ySpeed);
+            }
         }
         public float GetSpeed()
         {
