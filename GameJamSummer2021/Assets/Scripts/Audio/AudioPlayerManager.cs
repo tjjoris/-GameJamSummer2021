ed8a440bbb061c6aacc1f6d70c179204ed181b5e
tjjoris <tjjoris@gmail.com>
2021-07-03 17:00:30 -0400

    created audio player manager
    attempting to implement max speed

---
+++ GameJamSummer2021/Assets/Scripts/Audio/AudioPlayerManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FreeEscape
+{
+    public class AudioPlayerManager : MonoBehaviour
+    {
+        //[SerializeField] AudioSource bombFire;
+        //[SerializeField] AudioSource bonk;
+        //[SerializeField] AudioSource bonk2;
+        //[SerializeField] AudioSource bonk3;
+        //[SerializeField] AudioSource explosion2;
+        //[SerializeField] AudioSource explosion2a;
+        //[SerializeField] AudioSource explosion3;
+        //[SerializeField] AudioSource explosion3a;
+        //[SerializeField] AudioSource thruster1;
+        //[SerializeField] AudioSource thruster2;
+        //[SerializeField] AudioSource winTheme;
+        [SerializeField] AudioClip bombFireAC;
+        [SerializeField] AudioClip bonkAC;
+        [SerializeField] AudioClip bonk2AC;
+        [SerializeField] AudioClip bonk3AC;
+        [SerializeField] AudioClip explosion2AC;
+        [SerializeField] AudioClip explosion2aAC;
+        [SerializeField] AudioClip explosion3AC;
+        [SerializeField] AudioClip explosion3aAC;
+        [SerializeField] AudioClip thruster1AC;
+        [SerializeField] AudioClip thruster2AC;
+        [SerializeField] AudioClip winThemeAC;
+        
+
+        public void PlayBombFire()
+        {
+            
+        }
+    }
+}
