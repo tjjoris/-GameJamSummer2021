91b46b81bfc32398d9e1e0257a7ff0aa79b5a38f
Tars1n <mudd.rosal@gmail.com>
2021-07-03 16:23:51 -0400

    TestScene working with win screen upon clearing debris

--- GameJamSummer2021/Assets/Scripts/core/DebrisTracker.cs
+++ GameJamSummer2021/Assets/Scripts/core/DebrisTracker.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace FreeEscape.Core
 {
@@ -14,17 +15,20 @@ namespace FreeEscape.Core
         private List<DebrisEvent> debrisResistantList; //Harder to destroy debris such as spaceship parts
         private List<DebrisEvent> specialDebrisList; //Destructables with special behaviours, such as explosive ship reactor or corrosive alien eggs
         private int totalDebrisRemaining;
+        [SerializeField] private TextMeshProUGUI debrisRemainingText;
 
-        
-        // void Start()
-        // {
-        //     ObjectsToTrack(debrisBasicParent);
-        // }
+        private DebrisEvent debrisEvent;
+        public event EventHandler AllDebrisCleared;
 
-        public DebrisEvent debrisEvent;
-	
-        private void Start()
+        public void AlertEventWatchers()
         {
+		    AllDebrisCleared?.Invoke(this, EventArgs.Empty);
+        }
+	
+        public void TallyDebris()
+        {
+            totalDebrisRemaining = 0;
+
             debrisBasicList = new List<DebrisEvent>();
             debrisResistantList = new List<DebrisEvent>();
             specialDebrisList = new List<DebrisEvent>();
@@ -41,23 +45,42 @@ namespace FreeEscape.Core
                 DebrisEvent debrisEvent = debrisObj.gameObject.GetComponent<DebrisEvent>();
                 if (debrisEvent != null) 
                 {
+                    totalDebrisRemaining ++;
                     _list.Add(debrisEvent);
                     debrisEvent.DebrisDestroyedEvent += RegisterDebrisDestroyed;
                 }
+
+                UpdateDebrisText();
             }
+        }
+
+        private void UpdateDebrisText()
+        {
+            debrisRemainingText.text = "Debris Remaining: " + totalDebrisRemaining;
         }
         
         private void RegisterDebrisDestroyed (object sender, EventArgs e)
         {
-            Debug.Log("Debris Event Fired!");
-            //UnSubscribe(sender);
+            totalDebrisRemaining --;
+            UpdateDebrisText();
+            UnSubscribe((DebrisEvent)sender);
+
+            if (totalDebrisRemaining <= 0)
+            {
+                AllDebrisCleared?.Invoke(this, EventArgs.Empty);
+            }
         }
         
         
         private void UnSubscribe(DebrisEvent _debrisEvent)
         {
             //make sure to do this if script is ever removed
-            //_debrisEvent.DebrisDestroyedEvent -= RegisterDebrisDestroyed;
+            _debrisEvent.DebrisDestroyedEvent -= RegisterDebrisDestroyed;
+        }
+
+        public void LevelCleared()
+        {
+            debrisRemainingText.text = "All Debris Destroyed, Great Job!";
         }
     }
 }
