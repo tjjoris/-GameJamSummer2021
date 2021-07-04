8ba025c007d762f0a8a5c7b4abc801505ff32064
tjjoris <tjjoris@gmail.com>
2021-07-04 18:53:59 -0400

    audio set up mostly

--- GameJamSummer2021/Assets/Scripts/Damage/Debris.cs
+++ GameJamSummer2021/Assets/Scripts/Damage/Debris.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using FreeEscape.Display;
 using FreeEscape.Core;
+using FreeEscape.Audio;
 
 namespace FreeEscape.Damage
 {
@@ -12,11 +13,13 @@ namespace FreeEscape.Damage
         [SerializeField] private ProgressShader shader;
         [SerializeField] private float timeToVaporizeMin = 0.38f;
         [SerializeField] private float timeToVaporizeMax = 1.62f;
+        private AudioPlayerManager audioPlayerManager;
         private DebrisEvent debrisEvent;
 
         private void Start() 
         {
             debrisEvent = this.GetComponent<DebrisEvent>();
+            audioPlayerManager = FindObjectOfType<AudioPlayerManager>();
         }
  
         public void HitByExplosion(BombExplosion _explosion)
@@ -29,7 +32,7 @@ namespace FreeEscape.Damage
                 {
                     shader.ApplyShaderEffect(timeToVaporize);
                 }
-
+                //audioPlayerManager.PlayExplosion();
                 debrisEvent.AlertEventWatchers();    
                 Destroy(gameObject, timeToVaporize);
             }
