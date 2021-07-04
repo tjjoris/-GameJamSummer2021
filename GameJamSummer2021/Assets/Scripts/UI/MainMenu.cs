76f00e481658d2c87fe779aac5cd8047895f6b2d
Tars1n <mudd.rosal@gmail.com>
2021-07-04 10:03:37 -0400

    Added Options page w volume slider

--- GameJamSummer2021/Assets/Scripts/UI/MainMenu.cs
+++ GameJamSummer2021/Assets/Scripts/UI/MainMenu.cs
@@ -7,9 +7,23 @@ namespace FreeEscape.UI
 {
     public class MainMenu : MonoBehaviour
     {
+        [SerializeField] private GameObject mainMenuPage;
+        [SerializeField] private GameObject optionsPage;
         public void PlayGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void TravelToOptionsPage()
+        {
+            mainMenuPage.SetActive(false);
+            optionsPage.SetActive(true);
+        }
+
+        public void TravelToMainMenuPage()
+        {
+            mainMenuPage.SetActive(true);
+            optionsPage.SetActive(false);
         }
     }
 }
