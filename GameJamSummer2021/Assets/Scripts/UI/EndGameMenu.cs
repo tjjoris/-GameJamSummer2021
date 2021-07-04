ad00a70f0478139edf7e2f989f4587b1b67b7af8
Tars1n <mudd.rosal@gmail.com>
2021-07-04 19:03:13 -0400

    Menus should all link up, player death fixed

---
+++ GameJamSummer2021/Assets/Scripts/UI/EndGameMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace FreeEscape.UI
+{
+    public class EndGameMenu : MonoBehaviour
+    {
+        public void CompleteButton()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
--- GameJamSummer2021/Assets/Scripts/UI/GameplayMenu.cs
+++ GameJamSummer2021/Assets/Scripts/UI/GameplayMenu.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 namespace FreeEscape.UI
 {
@@ -10,12 +12,16 @@ namespace FreeEscape.UI
         public static bool GameIsPaused = false;
 
         [SerializeField] private GameObject pauseMenuUI;
-        [SerializeField] private GameObject clearAllScoreUI;
+        [SerializeField] private GameObject scoreScreenUI;
+        [SerializeField] private GameObject playerDestroyedScoreUI;
         [SerializeField] private GameObject outOfTimeScoreUI;
+        [SerializeField] private GameObject clearAllScoreUI;
+        [SerializeField] private TextMeshProUGUI scoreCardText;
+        [SerializeField] private Button nextLevelButton;
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape) && (!clearAllScoreUI.activeSelf && !outOfTimeScoreUI.activeSelf))
+            if (Input.GetKeyDown(KeyCode.Escape) && !scoreScreenUI.activeSelf)
             {
                 if (GameIsPaused)
                 {
@@ -30,36 +36,80 @@ namespace FreeEscape.UI
         public void ClosePauseMenu()
         {
             pauseMenuUI.SetActive(false);
-            Time.timeScale = 1f;
-            GameIsPaused = false;
+            ResumeTime();
         }
 
         public void OpenPauseMenu()
         {
             pauseMenuUI.SetActive(true);
+            PauseTime();
+        }
+
+        private void PauseTime()
+        {
             Time.timeScale = 0f;
             GameIsPaused = true;
         }
 
-        public void ClearAllDebrisScoreScreen()
+        private void ResumeTime()
         {
-            clearAllScoreUI.SetActive(true);
-            Time.timeScale = 0f;
-            GameIsPaused = true;
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+        }
+
+        public void PlayerDestroyedScoreScreen()
+        {
+            scoreScreenUI.SetActive(true);
+            playerDestroyedScoreUI.SetActive(true);
+            outOfTimeScoreUI.SetActive(false);
+            clearAllScoreUI.SetActive(false);
+            nextLevelButton.interactable = false;
+
+            PauseTime();
         }
 
         public void OutOfTimeScoreScreen()
         {
+            scoreScreenUI.SetActive(true);
+            playerDestroyedScoreUI.SetActive(false);
             outOfTimeScoreUI.SetActive(true);
-            Time.timeScale = 0f;
-            GameIsPaused = true;
+            clearAllScoreUI.SetActive(false);
+            nextLevelButton.interactable = true;
+            
+            PauseTime();
+        }
+        public void ClearAllDebrisScoreScreen()
+        {
+            scoreScreenUI.SetActive(true);
+            playerDestroyedScoreUI.SetActive(false);
+            outOfTimeScoreUI.SetActive(false);
+            clearAllScoreUI.SetActive(true);
+            nextLevelButton.interactable = true;
+            
+            PauseTime();
         }
 
         public void ExitToMainMenu()
         {
-            Time.timeScale = 1f;
-            GameIsPaused = false;
+            ResumeTime();
             SceneManager.LoadScene("MainMenu");
+        }
+
+        public void RetryLevelButton()
+        {
+            ResumeTime();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void NextLevelButton()
+        {
+            ResumeTime();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void WriteToScore(string _scoreText)
+        {
+            scoreCardText.text = _scoreText;
         }
     }
 }
