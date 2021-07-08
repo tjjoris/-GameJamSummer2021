b2b2e95c4006438f0ab70cd132b0d2b75c14cd34
tjjoris <tjjoris@gmail.com>
2021-07-08 00:27:23 -0400

    Added icons, starting to add feedback.

---
+++ GameJamSummer2021/Assets/Scripts/UI/I_ChangeBombActive.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FreeEscape.UI
+{
+    public interface I_ChangeBombActive
+    {
+        void BombActive();
+        void BombUnarmed();
+    }
+}
---
+++ GameJamSummer2021/Assets/Scripts/UI/StickyIcon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FreeEscape
+{
+    public class StickyIcon : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
