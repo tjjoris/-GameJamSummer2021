b4e673cee407631cc59feadb490d3eed29e9fd3e
Tars1n <mudd.rosal@gmail.com>
2021-11-27 15:15:09 -0500

    Odin Inspector added, SimpleAudioEvent working

--- GameJamSummer2021/Assets/Scripts/Audio/RangedFloat.cs
+++ GameJamSummer2021/Assets/Scripts/Audio/RangedFloat.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace FreeEscape
 {
     [System.Serializable]
     public class RangedFloat
     {
-        [Range(0f, 1f)]
-        public float Min = 0;
-        [Range(0f, 1f)]
-        public float Max = 1;
+        private Vector2 DynamicRange = new Vector2(0f,1f);
+        public float Min {get {return DynamicMinMax.x;}}
+        public float Max {get {return DynamicMinMax.y;}}
+
+        [MinMaxSlider("DynamicRange", true)]
+        public Vector2 DynamicMinMax = new Vector2(0.4f, 0.6f);
         
+        public void SetMinMaxRange(float _min, float _max)
+        {
+            DynamicRange.x = _min;
+            DynamicRange.y = _max;
+        }
     }
 }
--- GameJamSummer2021/Assets/Scripts/Audio/SimpleAudioEvent.cs
+++ GameJamSummer2021/Assets/Scripts/Audio/SimpleAudioEvent.cs
@@ -1,24 +1,48 @@
 using System.Collections;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using Random = UnityEngine.Random;
-
+using UnityEditor;
 namespace FreeEscape
 {
     [CreateAssetMenu(menuName = "Audio Events/Simple")]
     public class SimpleAudioEvent : AudioEvent
     {
+        private AudioSource _previewer; 
+        public void OnEnable()
+        {
+            _previewer = EditorUtility.CreateGameObjectWithHideFlags("Audio preview", HideFlags.HideAndDontSave, typeof(AudioSource)).GetComponent<AudioSource>();
+
+        }
+
+        public void OnDisable()
+        {
+            DestroyImmediate(_previewer.gameObject);
+        }
+
         [SerializeField] private AudioClip[] clips;
-        [SerializeField] private RangedFloat _volume;
-        //[Range(0,2)]
-        [SerializeField] private RangedFloat _pitch;
+
+        [PropertySpace(20)]
+        [MinMaxSlider(0,1, true)]
+        public Vector2 _volume = new Vector2(0.4f, 0.6f);
+        
+        [MinMaxSlider(0,2, true)]
+        public Vector2 _pitch = new Vector2(0.9f, 1.1f);
+
+        [PropertySpace(20)]
+        [Button(ButtonSizes.Gigantic)]
+        private void SampleAudioEvent()
+        {
+            Play(_previewer);
+        }
 
         public override void Play(AudioSource source)
         {
             if (clips.Length == 0) return;
 
             source.clip = clips[Random.Range(0, clips.Length)];
-            source.volume = Random.Range(_volume.Min, _volume.Max);
-            source.pitch = Random.Range(_pitch.Min, _pitch.Max);
+            source.volume = Random.Range(_volume.x, _volume.y);
+            source.pitch = Random.Range(_pitch.x, _pitch.y);
             source.Play();
         }
     }
